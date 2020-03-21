a59af82d78745d547de2b6d69e95ecd628757781
Amella Giuseppe <Pamellino@gmail.com>
2020-03-21 07:32:31 +0100

    helloworld

---
+++ TestMonoGame/Controlers/MenuPrincipal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMonoGame.mechanics {
+    class MenuPrincipal {
+        private ArrayList TextesMenus { get; set; }
+
+
+
+
+        public MenuPrincipal() {
+        TextesMenus = new ArrayList {
+                new string[] { "Nouvelle Partie", "Options", "Quitter" },
+                new string[] { "Graphiques", "Son", "Menu principal" }
+            };
+
+
+
+        }
+
+    }
+}
